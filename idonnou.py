34b7b3cd6195457c60655b9724f87596e62b9ef1
MiguelAlmodovar <53450442+MiguelAlmodovar@users.noreply.github.com>
2022-10-13 17:52:15 +0100

    Delete idonnou.py

--- idonnou.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, render_template, jsonify, request
-from models import *
-
-app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db.init_app(app)
